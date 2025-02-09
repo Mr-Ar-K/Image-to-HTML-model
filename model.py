fde8fff2bf0f7c5f0105904ef199fbc1ca8505be
Arun Kumar Korra <arunkumar.korraa@gmail.com>
2025-02-09 21:23:30 +0530

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,7 @@
+from transformers import AutoModelForSeq2SeqLM
+
+# Load pretrained BLIP model
+def load_model():
+    model = AutoModelForSeq2SeqLM.from_pretrained("Salesforce/blip-image-captioning-large")
+    return model
 
